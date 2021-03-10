563b2b08c3c5ecbc5064ff07313075d275d9e616
Vaibhav <vaibhavdangariay1234@gmail.com>
2021-03-10 13:36:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./Login.js";
+import { getTokenFromResponse } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from './Player'
+import {useDataLayerValue} from './DataLayer'
+const spotify = new SpotifyWebApi();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [{ token }, dispatch] = useDataLayerValue();
+
+  useEffect(() => {
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      spotify.setAccessToken(_token);
+       dispatch({
+        type: 'SET_TOKEN',
+        token: _token,
+      })
+
+      spotify.getMe().then(user => {
+      
+      dispatch({
+        type: 'SET_USER',
+        user: user,
+      })
+    });
+    spotify.getUserPlaylists().then((playlists) => {
+      dispatch({
+        type: "SET_PLAYLISTS",
+        playlists: playlists,
+      });
+    });
+    }
+    spotify.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+  }, []);
+
+  return <div>{token ? <Player spotify={spotify}/> : <Login />}</div>;
 }
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Login.css'
+import {accessUrl} from './spotify'
+function Login() {
+    return (
+       <div className="login">
+       {/* <h1>This is good</h1> */}
+         <img
+             src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+             alt=""
+         />
+        <a href={accessUrl}>Login with Spotify</a>
+        </div>
+    )
+}
+
+export default Login
